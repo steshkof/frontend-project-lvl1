72c18e556082ca47c6525047cb63a87c5e2af237
steshkof <steshkof@gmail.com>
2022-05-09 10:56:23 +0300

    step 4.1

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
+import { greet } from '../src/cli.js';
 
-import readlineSync from 'readline-sync';
-import {greet} from '../src/cli.js';
-
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 greet();
